807faa1e73d9d2255aec8052cfe235825557beb1
srimantika <srimantika@gmail.com>
2021-11-29 19:17:32 +0000

    created login functionality for restaurant owner

--- routes/login.js
+++ routes/login.js
@@ -3,15 +3,38 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
+
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then((data) => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
+
+        res.render("login");
+
+  });
+
+  router.post("/", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.query(`SELECT * FROM restaurants;`)
+    .then((data) => {
+      const users = data.rows[0];
+      console.log(users);
+      const usernamedb = users.username;
+      const passworddb = users.password;
+      console.log(usernamedb);
+      console.log(passworddb);
+      if (username === usernamedb && password === passworddb) {
+        res.redirect("resturantPage");
+       }
+      else {
+        res.status(404).send("Please enter a valid username and password!");
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
   });
   return router;
 };
+
+
+
