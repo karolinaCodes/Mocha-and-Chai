3de653adab7512daed6bcfefd45a7eca1f672f02
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-11-29 11:46:01 -0500

    routing adjustments

--- routes/orderPage.js
+++ routes/orderPage.js
@@ -2,21 +2,18 @@
 //  Create webpage for the customer for Orders
 // ======================================================
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM products;`)
-      .then(data => {
+      .then((data) => {
         const menu = data.rows;
         res.json({ menu });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
   return router;
