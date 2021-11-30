d253e56cb330d9b17b7ca1bf50d96eb27d263a54
srimantika <srimantika@gmail.com>
2021-11-30 04:22:12 +0000

    Restaurant owner page added with order details populated dynamically from database

--- routes/resturantPage.js
+++ routes/resturantPage.js
@@ -7,10 +7,16 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM customers;`)
+    db.query(`SELECT customers.first_name as first_name, customers.last_name as last_name, customers.email as email, customers.phone_no as phone_no, orders.id as orders_id, orders.status as status, order_details.id as line_id, orders.date as order_date, orders.total as total, products.title as product_name
+    FROM orders
+    JOIN customers ON customers.id = customer_id
+    JOIN order_details ON orders.id = order_id
+    JOIN products on products.id = product_id
+    ORDER BY orders.id;`)
       .then((data) => {
-        const menu = data.rows;
-        res.render("resturantPage");
+        const order_details = data.rows;
+        const templateVars = { dataArray: order_details };
+        res.render("resturantPage", templateVars);
       })
       .catch((err) => {
         res.status(500).json({ error: err.message });
