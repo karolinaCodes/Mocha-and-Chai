e4022d2066f4132fb74d13a9af3cad57103e2c13
srimantika <srimantika@gmail.com>
2021-11-30 20:47:52 +0000

    Restaurant owner post functions with accept/reject order and updating estimation time

--- routes/resturantPage.js
+++ routes/resturantPage.js
@@ -4,23 +4,61 @@
 
 const express = require("express");
 const router = express.Router();
+//const client = require('twilio')(process.env.ACCOUNTSID ,process.env.AUTHTOKEN);
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    db.query(`SELECT customers.first_name as first_name, customers.last_name as last_name, customers.email as email, customers.phone_no as phone_no, orders.id as orders_id, orders.status as status, order_details.id as line_id, orders.date as order_date, orders.total as total, products.title as product_name
+    db.query(`SELECT customers.first_name as first_name, customers.last_name as last_name, customers.email as email,
+    customers.phone_no as phone_no, orders.id as orders_id, order_details.qty as qty, orders.status as status, order_details.id as line_id,
+    orders.date as order_date, orders.total as total, products.title as product_name
     FROM orders
     JOIN customers ON customers.id = customer_id
     JOIN order_details ON orders.id = order_id
     JOIN products on products.id = product_id
+    WHERE orders.status = 'Placed'
     ORDER BY orders.id;`)
       .then((data) => {
         const order_details = data.rows;
-        const templateVars = { dataArray: order_details };
+        const arrayLength = order_details.length;
+        const templateVars = { dataArray: order_details, length : arrayLength };
         res.render("resturantPage", templateVars);
       })
       .catch((err) => {
         res.status(500).json({ error: err.message });
       });
   });
+
+  router.post("/", (req, res) => {
+    const estimationtime = req.body.estimatedtime;
+    const orderid = req.body.order_no;
+    const accept = req.body.acceptButton;
+    const reject = req.body.rejectButton;
+
+   if (accept) {
+      db.query(`UPDATE orders
+                    SET status = 'Accepted',
+                        estimated_time = ${estimationtime}
+                    WHERE id = ${orderid};`)
+        .then((data) => {
+          res.redirect("resturantPage");
+        })
+        .catch((err) => {
+          res.status(500).json({ error: err.message });
+        });
+    } else if (reject) {
+        db.query(`UPDATE orders
+        SET status = 'Rejected'
+        WHERE id = ${orderid};`)
+        .then((data) => {
+        res.redirect("resturantPage");
+        })
+        .catch((err) => {
+        res.status(500).json({ error: err.message });
+        });
+   }
+});
+
   return router;
 };
+
+
