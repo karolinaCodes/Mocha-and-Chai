1a0f3eb7775a131cf0d96d35bc5e9f741cee22fc
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-12-01 14:19:14 -0500

    add footer to login and orderPage

--- routes/prepTime.js
+++ routes/prepTime.js
@@ -7,7 +7,6 @@ const router = express.Router();
 module.exports = (db) => {
   router.get("/:order_id", (req, res) => {
     const { order_id } = req.params;
-    // console.log("order_id", order_id);
 
     db.query(
       `SELECT estimated_time
